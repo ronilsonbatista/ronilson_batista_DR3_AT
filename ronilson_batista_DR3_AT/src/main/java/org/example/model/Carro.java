7cbcc565470a61e3a22803f715c0f48d229047da
Ronilson da Conceição Batista <ronilsondaconceicao@gmail.com>
2024-09-26 00:04:29 -0300

    carro service updates

--- ronilson_batista_DR3_AT/src/main/java/org/example/model/Carro.java
+++ ronilson_batista_DR3_AT/src/main/java/org/example/model/Carro.java
@@ -1,8 +1,8 @@
 package org.example.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Data
 @NoArgsConstructor
--- ronilson_batista_DR3_AT/src/main/java/org/example/service/CarroService.java
+++ ronilson_batista_DR3_AT/src/main/java/org/example/service/CarroService.java
@@ -1,30 +1,62 @@
 package org.example.service;
 
+import org.example.dto.CarroDTOInput;
+import org.example.dto.CarroDTOOutput;
 import org.example.model.Carro;
+import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class CarroService {
 
     private List<Carro> listaCarros = new ArrayList<>();
+    private ModelMapper modelMapper = new ModelMapper();
 
-    public List<Carro> listar() {
-        return listaCarros;
+
+    public List<CarroDTOOutput> listar() {
+        List<CarroDTOOutput> lista = new ArrayList<>();
+        for (Carro carro : listaCarros) {
+            CarroDTOOutput carroDTO = modelMapper.map(carro, CarroDTOOutput.class);
+            lista.add(carroDTO);
+        }
+        return lista;
     }
 
-    public void inserir(Carro carro) {
+    public void inserir(CarroDTOInput dto) {
+        Carro carro = modelMapper.map(dto, Carro.class);
         listaCarros.add(carro);
     }
 
-    public List<Carro> buscar(int id) {
-        return listaCarros;
+    public boolean alterar(int id, CarroDTOInput dto) {
+        for (int i = 0; i < listaCarros.size(); i++) {
+            Carro carro = listaCarros.get(i);
+            if (carro.getId() == id) {
+                Carro carroAlterado = modelMapper.map(dto, Carro.class);
+                carroAlterado.setId(id);
+                listaCarros.set(i, carroAlterado);
+                return true;
+            }
+        }
+        return false;
     }
 
-    public void alterar(int id) {
+    public CarroDTOOutput buscar(int id) {
+        for (Carro carro : listaCarros) {
+            if (carro.getId() == id) {
+                return modelMapper.map(carro, CarroDTOOutput.class);
+            }
+        }
+        return null;
     }
 
-    public void excluir(int id) {
+    public boolean excluir(int id) {
+        for (int i = 0; i < listaCarros.size(); i++) {
+            if (listaCarros.get(i).getId() == id) {
+                listaCarros.remove(i);
+                return true;
+            }
+        }
+        return false;
     }
 }
